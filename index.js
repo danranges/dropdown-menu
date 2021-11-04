1646d16d9e7c31483815167af657b568dc8177e0
Dan Ranges <danranges@gmail.com>
2021-11-04 18:15:43 +0100

    Add HTML, styles. Make js modular

---
+++ index.js
@@ -0,0 +1,21 @@
+function addDropdownEvents(dropdown, button) {
+  document.addEventListener('click', (e) => {
+    const isDropdownButton = e.target.matches(`[${button}]`);
+
+    if (!isDropdownButton && e.target.closest(`[${dropdown}]`)) return;
+
+    let currentDropdown;
+    if (isDropdownButton) {
+      currentDropdown = e.target.closest(`[${dropdown}]`);
+      currentDropdown.classList.toggle('active');
+    }
+
+    document.querySelectorAll(`[${dropdown}]`).forEach((dropdown) => {
+      if (dropdown !== currentDropdown) {
+        dropdown.classList.remove('active');
+      }
+    });
+  });
+}
+
+addDropdownEvents('data-dropdown', 'data-dropdown-button');
